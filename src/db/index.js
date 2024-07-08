1ce1b12433dc42a3d53d1a867b6a601b14b1991c
bishalsubed <142403372+bishalsubed@users.noreply.github.com>
2024-07-08 15:12:53 +0545

    utils

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+
+const connectDb = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connected")
+    } catch (error) {
+        console.error(`MongoDb connection failed : ${error}`)
+        process.exit(1)
+    }
+}
+
+export default connectDb;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import { app } from "./app.js";
+import connectDb from "./db/index.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at the port ${process.env.PORT} `)
+    })
+})
+.catch((err)=>{"MONGODB connection failed!",err})
